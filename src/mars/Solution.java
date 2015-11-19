c7353f85fbf35aee7feaab0d5a0743f838417f08
Sybil-Li <sybil.lbq.acc@gmail.com>
2015-11-19 16:40:04 +0000

    Local search to find path first commit

---
+++ src/mars/Solution.java
@@ -0,0 +1,23 @@
+package mars;
+
+import java.util.AbstractMap.SimpleEntry;
+import java.util.LinkedList;
+
+public class Solution {
+	public void main (String[] args) {
+		Planet p = new Planet();
+		
+		SimpleEntry<Integer, Integer> start = new SimpleEntry<Integer, Integer>(4,4);
+		
+		// best result for local search
+		LinkedList<SimpleEntry<Integer, Integer>> res = new LinkedList<SimpleEntry<Integer, Integer>>();		
+		
+		// frontier
+		LinkedList<SimpleEntry<Integer, Integer>> frontier = new LinkedList<SimpleEntry<Integer, Integer>>();
+		
+		
+		
+	}
+	
+
+}
